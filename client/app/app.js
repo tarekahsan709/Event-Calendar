65c6bb4d97a162d67a9a8d79c23bee3e10fcdffe
Md. Nazmul Ahsan <tarekahsan709@gmail.com>
2017-10-14 00:44:41 +0600

    add demo code of calender

--- client/app/app.js
+++ client/app/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import angular from 'angular';
-// import ngAnimate from 'angular-animate';
+import ngAnimate from 'angular-animate';
 import ngCookies from 'angular-cookies';
 import ngResource from 'angular-resource';
 import ngSanitize from 'angular-sanitize';
@@ -27,7 +27,7 @@ import socket from '../components/socket/socket.service';
 import './app.css';
 
 angular.module('jeeonApp', [
-    // ngAnimate,
+    ngAnimate,
     ngCookies, ngResource, ngSanitize, 'btford.socket-io', uiRouter, uiBootstrap, mwl_calendar,
     // ngMessages,
     navbar, footer, main, constants, socket, util
--- client/app/main/main.component.js
+++ client/app/main/main.component.js
@@ -3,73 +3,151 @@ import uiRouter from 'angular-ui-router';
 import routing from './main.routes';
 
 export class MainController {
-  $http;
-  socket;
-  awesomeThings = [];
-  newThing = '';
+    $http;
+    socket;
+    awesomeThings = [];
+    newThing = '';
 
-  /*@ngInject*/
-  constructor($http, $scope, socket) {
-    this.$http = $http;
-    this.socket = socket;
-      this.viewDate = new Date();
-
-    $scope.$on('$destroy', function() {
-      socket.unsyncUpdates('thing');
-    });
-  }
-
-  $onInit() {
-    this.calendarView = 'month';
-      this.events = [
-          {
-              title: 'My event title', // The title of the event
-              startsAt: new Date(2013,5,1,1), // A javascript date object for when the event starts
-              endsAt: new Date(2014,8,26,15), // Optional - a javascript date object for when the event ends
-              color: { // can also be calendarConfig.colorTypes.warning for shortcuts to the deprecated event types
-                  primary: '#e3bc08', // the primary event color (should be darker than secondary)
-                  secondary: '#fdf1ba' // the secondary event color (should be lighter than primary)
-              },
-              actions: [{ // an array of actions that will be displayed next to the event title
-                  label: '<i class=\'glyphicon glyphicon-pencil\'></i>', // the label of the action
-                  cssClass: 'edit-action', // a CSS class that will be added to the action element so you can implement custom styling
-                  onClick: function(args) { // the action that occurs when it is clicked. The first argument will be an object containing the parent event
-                      console.log('Edit event', args.calendarEvent);
-                  }
-              }],
-              draggable: true, //Allow an event to be dragged and dropped
-              resizable: true, //Allow an event to be resizable
-              incrementsBadgeTotal: true, //If set to false then will not count towards the badge total amount on the month and year view
-              recursOn: 'year', // If set the event will recur on the given period. Valid values are year or month
-              cssClass: 'a-css-class-name', //A CSS class (or more, just separate with spaces) that will be added to the event when it is displayed on each view. Useful for marking an event as selected / active etc
-              allDay: false // set to true to display the event as an all day event on the day view
-          }
-      ];
-    this.$http.get('/api/things')
-      .then(response => {
-        this.awesomeThings = response.data;
-        this.socket.syncUpdates('thing', this.awesomeThings);
-      });
-  }
-
-  addThing() {
-    if (this.newThing) {
-      this.$http.post('/api/things', {
-        name: this.newThing
-      });
-      this.newThing = '';
+    /*@ngInject*/
+    constructor($http, $scope, socket, moment, calendarConfig) {
+        this.$http = $http;
+        this.moment = moment;
+        this.calendarConfig = calendarConfig;
+        this.socket = socket;
+        $scope.$on('$destroy', function () {
+            socket.unsyncUpdates('thing');
+        });
     }
-  }
 
-  deleteThing(thing) {
-    this.$http.delete('/api/things/' + thing._id);
-  }
+    $onInit() {
+        //These variables MUST be set as a minimum for the calendar to work
+        this.calendarView = 'month';
+        this.viewDate = new Date();
+        var actions = [{
+            label: '<i class=\'glyphicon glyphicon-pencil\'>edif</i>',
+            onClick: function (args) {
+                alert('actions 1');
+                alert.show('Edited', args.calendarEvent);
+            }
+        }, {
+            label: '<i class=\'glyphicon glyphicon-remove\'>remove</i>',
+            onClick: function (args) {
+                alert('actions 2');
+                alert.show('Deleted', args.calendarEvent);
+            }
+        }];
+        this.events = [
+            {
+                title: 'An event',
+                color: this.calendarConfig.colorTypes.warning,
+                startsAt: this.moment().startOf('week').subtract(2, 'days').add(8, 'hours').toDate(),
+                endsAt: this.moment().startOf('week').add(1, 'week').add(9, 'hours').toDate(),
+                draggable: true,
+                resizable: true,
+                actions: actions
+            }, {
+                title: '<i class="glyphicon glyphicon-asterisk"></i> <span class="text-primary">Another event</span>, with a <i>html</i> title',
+                color: this.calendarConfig.colorTypes.info,
+                startsAt: this.moment().subtract(1, 'day').toDate(),
+                endsAt: this.moment().add(5, 'days').toDate(),
+                draggable: true,
+                resizable: true,
+                actions: actions
+            }, {
+                title: 'This is a really long event title that occurs on every year',
+                color: this.calendarConfig.colorTypes.important,
+                startsAt: this.moment().startOf('day').add(7, 'hours').toDate(),
+                endsAt: this.moment().startOf('day').add(19, 'hours').toDate(),
+                recursOn: 'year',
+                draggable: true,
+                resizable: true,
+                actions: actions
+            }
+        ];
+        this.cellIsOpen = true;
+    }
+
+    addThing() {
+        if (this.newThing) {
+            this.$http.post('/api/things', {
+                name: this.newThing
+            });
+            this.newThing = '';
+        }
+    }
+
+    deleteThing(thing) {
+        this.$http.delete('/api/things/' + thing._id);
+    }
+
+    addEvent() {
+        this.events.push({
+            title: 'New event',
+            startsAt: this.moment().startOf('day').toDate(),
+            endsAt: this.moment().endOf('day').toDate(),
+            color: this.calendarConfig.colorTypes.important,
+            draggable: true,
+            resizable: true
+        });
+    };
+
+    eventClicked(event) {
+        alert('eventClicked');
+        alert.show('Clicked', event);
+    };
+
+    eventEdited(event) {
+        alert('eventEdited');
+
+        alert.show('Edited', event);
+    };
+
+    eventDeleted(event) {
+        alert('eventDeleted');
+
+        alert.show('Deleted', event);
+    };
+
+    eventTimesChanged(event) {
+        alert('eventTimesChanged');
+        alert.show('Dropped or resized', event);
+    };
+
+    toggle($event, field, event) {
+        alert('toggle');
+
+        $event.preventDefault();
+        $event.stopPropagation();
+        event[field] = !event[field];
+    };
+
+    timespanClicked(date, cell) {
+        alert('timespanClicked');
+
+        if (this.calendarView === 'month') {
+            if ((this.cellIsOpen && this.moment(date).startOf('day').isSame(this.moment(this.viewDate).startOf('day'))) || cell.events.length === 0 || !cell.inMonth) {
+                this.cellIsOpen = false;
+            } else {
+                this.cellIsOpen = true;
+                this.viewDate = date;
+            }
+        } else if (this.calendarView === 'year') {
+            if ((this.cellIsOpen && this.moment(date).startOf('month').isSame(this.moment(this.viewDate).startOf('month'))) || cell.events.length === 0) {
+                this.cellIsOpen = false;
+            } else {
+                this.cellIsOpen = true;
+                this.viewDate = date;
+            }
+        }
+
+    };
+
 }
 
 export default angular.module('jeeonApp.main', [uiRouter])
-  .config(routing)
-  .component('main', {
-    template: require('./main.html'),
-    controller: MainController
-  })
-  .name;
+    .config(routing)
+    .component('main', {
+        template: require('./main.html'),
+        controller: MainController
+    })
+    .name;
